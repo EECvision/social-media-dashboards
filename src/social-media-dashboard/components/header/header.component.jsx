6714a929840541c114c742cae1b34d54394bf1bb
Emmanuel <eec.studies@gmail.com>
2021-01-05 18:39:46 +0100

    adjusted the header width to fit in the mobile devices

--- src/social-media-dashboard/components/header/header.component.jsx
+++ src/social-media-dashboard/components/header/header.component.jsx
@@ -8,11 +8,12 @@ const DashboardHeader=()=>{
 
     return(
         <div className={`w-full h-64 ${componentBg} flex justify-center items-start`}>
-            <div className="w-full md:max-w-6xl flex md:flex-row flex-col justify-between md:items-center items-between mt-8 px-32 md:px-6">
+            <div className="w-full md:max-w-6xl flex md:flex-row flex-col justify-between md:items-center items-between mt-8 px-8 md:px-6">
                 <div className="mb-4">
                     <h1 className={`font-bold text-${mainText} text-xl md:text-2xl`}>Social Media Dashboard</h1>
                     <p className={`text-${subText} text-sm`}>Total Followers: 23,004</p>
                 </div>
+                <div className="sm:hidden border-t border-gray-500 py-2"></div>
                 <div className="flex justify-between md:justify-center items-center">
                     <div className={`text-${subText} text-sm mr-2`}>Dark mode</div>
                     <ThemeSwitch/>
